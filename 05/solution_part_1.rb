9ee1aa32c110eae55a1275c8d09bc4fa664d79fa
Salazar <github@salazarmx.com>
2024-12-05 23:56:16 -0600

    Struggling with part 2... tired, sleepy

--- 05/solution_part_1.rb
+++ 05/solution_part_1.rb
@@ -1,6 +1,7 @@
+require "debug"
 require_relative "update"
 
-input = File.read("input.txt").split("\n")
+input = File.read("test_input.txt").split("\n")
 
 rules = {}
 updates = []
@@ -20,15 +21,55 @@ updates.each do |update|
   update.instructions.each_with_index do |instruction, index|
     next unless rules[instruction]
     
-    previous_instructions = update.instructions[0..index]
+    previous_instructions = update.instructions[0...index]
 
     if previous_instructions.intersect?(rules[instruction])
       update.right_order = false
+      update.broken_rule = "#{instruction}|#{previous_instructions[-1]}"
       break
     end
   end
 end
 
-puts "Total updates: #{updates.length}"
-puts "Updates in right order: #{updates.select(&:right_order?).length}"
-puts "Total sum of their middle_pages: #{updates.select(&:right_order?).sum(&:middle_page)}"
+wrong_updates = updates.select { |update| !update.right_order? }
+
+wrong_updates.each do |update|
+  puts "Fixing [#{update.instructions.join(", ")}]"
+  while !update.right_order?
+    if update.broken_rule
+      puts "Fixing rule: #{update.broken_rule}"
+      rule, offending_instruction = update.broken_rule.split("|")
+      
+      # Shift the offending instruction to the right place
+      removed = update.instructions.delete(offending_instruction)
+      update.instructions.insert(update.instructions.index(rule) + 2, removed)
+      update.instructions.compact!
+    end
+
+    # Check if the order is correct now
+    # TODO: TRAVERSE THE ARRAY IN REVERSE
+    fixed = update.instructions.cycle.each_with_index.map do |instruction, index|
+      next unless rules[instruction]
+      actual_index = update.instructions.index(instruction)
+      puts "Checking #{instruction} against #{update.instructions[0...actual_index].join(", ")}"
+
+      previous_instructions = update.instructions[0...actual_index]
+
+      if !previous_instructions.intersect?(rules[instruction])
+        update.right_order = true
+        update.broken_rule = nil
+        is_correct = true
+      else
+        is_correct = false
+        update.right_order = false
+        update.broken_rule = "#{instruction}|#{previous_instructions[-1]}"
+      end
+
+      is_correct
+    end.all?
+
+    break if fixed
+  end
+end 
+
+debugger
--- 05/update.rb
+++ 05/update.rb
@@ -1,11 +1,11 @@
 class Update
-  attr_reader :instructions, :middle_page
-  attr_accessor :right_order
+  attr_accessor :right_order, :broken_rule, :instructions, :middle_page
 
   def initialize(instructions)
     @instructions = Array(instructions)
     @right_order = true
-    @middle_page ||= get_middle_page
+    @middle_page = get_middle_page
+    @broken_rule = nil
   end
 
   def right_order?
